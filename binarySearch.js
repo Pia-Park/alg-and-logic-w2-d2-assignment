8de506b5b8b2e9fb3c3dc6b1eb19efec4f3dd0a8
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2020-09-22 11:50:01 -0700

    changed answer(learn from aidan)

--- binarySearch.js
+++ binarySearch.js
@@ -2,22 +2,34 @@
 // the function will also track, and print out the number of steps required to reach the target
 // the index of the item 
 
-let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 15, 16, 19, 24, 26, 27, 29]
+let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 15, 16, 19, 24, 29, 27, 26]
 
 const binarySearch = function(arr, target) {
-    let lowIndex = 0;
-    let highIndex = arr.length - 1;
+ 
+    // Sort value.
+    arr.sort((a, b) => a - b);
+
+    let left = 0; // first index of array
+    let right = arr.length - 1; // last index of array
+    let counter = 0;
     
-    while(lowIndex <= highIndex){
-        let midIndex = Math.floor((lowIndex + highIndex) / 2);
-        if (arr[midIndex] == target){
-            return midIndex;
-        } else if(arr[midIndex] > target){
-            highIndex = midIndex - 1;
+
+    while(left <= right) {
+        // Calculate middle value of sorted array.
+        middle = Math.floor((left + right) / 2);
+        counter += 1;
+        if(arr[middle] === target) {
+            index = middle;
+            // console.log('track number ' + arr[middle])
+            return `index = ${index} count = ${counter}`;
+        } else if (arr[middle] < target) {
+            // console.log('track number ' + arr[middle])
+            left = middle + 1;
         } else {
-            lowIndex = midIndex + 1;
+            // console.log('track number ' + arr[middle])
+            right = middle - 1;
         }
-    } 
+    }
 };
 
 
--- linearSearch.js
+++ linearSearch.js
@@ -1,11 +1,13 @@
 // Write an algorithm that performs linear search on a given array.
 
 let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 15, 16, 19, 24, 26, 27, 29]
+let counter = 0;
 
 const linearSearch = function(arr, target) {
+    counter += 1;
     for(let i = 0; i < arr.length; i++){
         if(arr[i] == target){
-            return i;
+            return ('index = ' + i + ', counter = '+ counter);
         } 
     }
 
