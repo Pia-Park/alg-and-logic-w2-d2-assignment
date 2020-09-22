524c9e5c12c9abb759bfdf969a757e269969d4ad
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2020-09-22 09:56:22 -0700

    checked

--- binarySearch.js
+++ binarySearch.js
@@ -2,10 +2,22 @@
 // the function will also track, and print out the number of steps required to reach the target
 // the index of the item 
 
-let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 15, 16, 19, 24, 29, 27, 26]
+let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 15, 16, 19, 24, 26, 27, 29]
 
 const binarySearch = function(arr, target) {
-
+    let lowIndex = 0;
+    let highIndex = arr.length - 1;
+    
+    while(lowIndex <= highIndex){
+        let midIndex = Math.floor((lowIndex + highIndex) / 2);
+        if (arr[midIndex] == target){
+            return midIndex;
+        } else if(arr[midIndex] > target){
+            highIndex = midIndex - 1;
+        } else {
+            lowIndex = midIndex + 1;
+        }
+    } 
 };
 
 
--- linearSearch.js
+++ linearSearch.js
@@ -1,8 +1,13 @@
 // Write an algorithm that performs linear search on a given array.
 
-let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 15, 16, 19, 24, 29, 27, 26]
+let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 15, 16, 19, 24, 26, 27, 29]
 
 const linearSearch = function(arr, target) {
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] == target){
+            return i;
+        } 
+    }
 
 };
 
